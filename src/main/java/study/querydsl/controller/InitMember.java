2f76dd99ce71cd8eebc8985f585ac6540877bbf8
엄태권 <akfls221@gmail.com>
2022-05-19 02:05:41 +0900

    Controller 생성 yml파일 설정 retry

---
+++ src/main/java/study/querydsl/controller/InitMember.java
@@ -0,0 +1,42 @@
+package study.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Profile("local")
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    public static class InitMemberService {
+        @PersistenceContext
+        private EntityManager em;
+
+        @Transactional
+        public void init() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+
+            for (int i = 0; i < 100; i++) {
+                Team selectTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member"+i, i, selectTeam));
+            }
+        }
+    }
+
+}
