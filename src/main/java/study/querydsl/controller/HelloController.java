36c514d37538a8610450a19166c2cf475b9c0c17
엄태권 <akfls221@gmail.com>
2022-05-24 01:44:47 +0900

    스프링 데이터 - Controller로 확인

--- src/main/java/study/querydsl/controller/HelloController.java
+++ src/main/java/study/querydsl/controller/HelloController.java
@@ -1,13 +1,31 @@
 package study.querydsl.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/test") //http://localhost:8081/test?page=2&size=10
+    public Page<MemberTeamDto> searchMember(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
